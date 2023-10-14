b21e6ee5387dc7f323c41b58fbf1ad5ea67f33d4
ThonAtaide <thon_ataide10@hotmail.com>
2023-10-14 00:03:45 -0300

    commit to create project

---
+++ src/main/java/br/com/financialtoolapi/FinancialToolApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.financialtoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialToolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialToolApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/financialtoolapi/FinancialToolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financialtoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialToolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
